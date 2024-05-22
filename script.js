a3fdcc4f59db70ac1a086f46cbbad4c81c382011
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-22 14:55:49 +0200

    added movies array

---
+++ script.js
@@ -0,0 +1,47 @@
+const movies = [
+    {
+        title: 'Inception',
+        year: 2010,
+        genre: 'Sci-Fi',
+        rating: 8.8,
+        type: 'movie',
+    },
+    {
+        title: 'Breaking Bad',
+        year: 2008,
+        genre: 'Crime',
+        rating: 9.5,
+        type: 'tv',
+        seasons: 5
+    },
+    {
+        title: 'The Matrix',
+        year: 1999,
+        genre: 'Sci-Fi',
+        rating: 8.7,
+        type: 'movie',
+    },
+    {
+        title: 'Stranger Things',
+        year: 2016,
+        genre: 'Drama',
+        rating: 8.8,
+        type: 'tv',
+        seasons: 4
+    },
+    {
+        title: 'The Godfather',
+        year: 1972,
+        genre: 'Crime',
+        rating: 9.2,
+        type: 'movie',
+    },
+    {
+        title: 'Game of Thrones',
+        year: 2011,
+        genre: 'Fantasy',
+        rating: 9.3,
+        type: 'tv',
+        seasons: 8
+    }
+]
