2fd4706e9e3e33904be45c459cde6c9232e82ab1
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-22 15:23:25 +0200

    added info Movie and TvSerie

--- script.js
+++ script.js
@@ -56,11 +56,22 @@ class Movie {
         this.rating = rating;
         this.type = type;
     }
+
+    // Info film
+    toString() {
+        return `${this.title} è un film di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${rating}.`
+    }
 }
 
+// Nuova classe TvSerie
 class TvSerie extends Movie {
     constructor(title, year, genre, rating, seasons) {
         super(title, year, genre, rating, 'tv');
         this.seasons = seasons;
+    }
+
+    // Info TvSerie
+    toString() {
+        return `${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciato nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}.`
     }
 }
